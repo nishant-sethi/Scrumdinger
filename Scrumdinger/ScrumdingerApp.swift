43f654bfbefb2e9175dfb7ae0094b4dfecf0ce04
nishantsethi <nsethi610@gmail.com>
2023-12-01 16:41:27 +0000

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Nishant sethi on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
